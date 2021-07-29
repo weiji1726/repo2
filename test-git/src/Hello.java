332eba98f8cadc5098b9d39be3ee50d9cb8de29b
lfc <2453692113@qq.com>
2021-07-29 17:09:16 +0800

    添加hello类

---
+++ test-git/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * @Author Wisdom
+ * @Date 2021/7/29
+ */
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
