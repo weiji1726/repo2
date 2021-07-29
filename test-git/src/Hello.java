c9ba6d35dda1a7b9a35ae3d988a33695ff05eaa2
lfc <2453692113@qq.com>
2021-07-29 19:59:59 +0800

    修改多处

--- test-git/src/Hello.java
+++ test-git/src/Hello.java
@@ -13,15 +13,27 @@ public class Hello{
     public void fun(){
         System.out.println("修改第一行");
         System.out.println("新增方法fun");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
         System.out.println("修改fun第三行");
     }
     public void fun1(){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
         System.out.println("新增方法fun1");
     }
     public void fun3(){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
         System.out.println("新增方法");
     }
     public void fun2(){
         System.out.println("新增方法");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
